71d6a26692e9b2653395ff18cd29b452e80eca9f
Michael Axiak <mike@axiak.net>
2010-07-22 00:05:35 -0400

    Added more setup.py antics

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all:
 	rm -rf build &>/dev/null
 	rm -rf src/*.so &>/dev/null
-	rm -rf src/re2.cpp &>/dev/null
-	python setup.py build_ext --inplace
+	rm -rf re2.so &>/dev/null
+	#rm -rf src/re2.cpp &>/dev/null
+	python setup.py build_ext
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import os
 from distutils.core import setup, Extension, Command
 
 class TestCommand(Command):
@@ -29,16 +30,43 @@ else:
     ext_files.append("src/re2.cpp")
 
 
+# Locate the re2 module
+_re2_prefixes = [
+    '/usr',
+    '/usr/local',
+    '/opt/',
+    ]
+
+for re2_prefix in _re2_prefixes:
+    if os.path.exists(os.path.join(re2_prefix, "include", "re2")):
+        break
+else:
+    raise OSError("Cannot find RE2 library. Please install it from http://code.google.com/p/re2/wiki/Install")
+
+
 setup(
     name="re2",
     version="0.2.0",
     description="Python wrapper for Google's RE2 using Cython",
     author="Mike Axiak",
+    license="New BSD License",
+    author_email = "mike@axiak.net",
+    url = "http://github.com/axiak/pyre2/",
+    description = "In-place re-replacement python extension for re2",
     ext_modules = [Extension("re2",
                              ext_files,
                              language="c++",
-                             include_dirs=["/usr/include/re2"],
+                             include_dirs=[os.path.join(re2_prefix, "include", "re2")],
                              libraries=["re2"],
+                             library_dirs=[os.path.join(re2_prefix, "lib")],
                              )],
     cmdclass=cmdclass,
+    classifiers = [
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
     )
