6222fe991320aaa33fc2bf156fdd9c08489d67de
Michael Axiak <mike@axiak.net>
2011-12-07 14:51:18 -0500

    Added version check in setup.py to prevent people from shooting themselves in the foot trying to compile with an old cython version.

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import os
 import re
 from distutils.core import setup, Extension, Command
 
+MINIMUM_CYTHON_VERSION = '0.13'
+
 class TestCommand(Command):
     description = 'Run packaged tests'
     user_options = []
@@ -17,12 +19,22 @@ class TestCommand(Command):
         from tests import re2_test
         re2_test.testall()
 
+
+def version_compare(version1, version2):
+    def normalize(v):
+        return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+    return cmp(normalize(version1), normalize(version2))
+
 cmdclass = {'test': TestCommand}
 
 ext_files = []
 if '--cython' in sys.argv[1:]:
     # Using Cython
     sys.argv.remove('--cython')
+    from Cython.Compiler.Main import Version
+    if version_compare(MINIMUM_CYTHON_VERSION, Version.version) > 0:
+        raise ValueError("Cython is version %s, but needs to be at least %s." %
+                         (Version.version, MINIMUM_CYTHON_VERSION))
     from Cython.Distutils import build_ext
     cmdclass['build_ext'] = build_ext
     ext_files.append("src/re2.pyx")
@@ -59,30 +71,34 @@ def get_authors():
     authors_f.close()
     return ', '.join(authors)
 
-setup(
-    name="re2",
-    version="0.2.20",
-    description="Python wrapper for Google's RE2 using Cython",
-    long_description=get_long_description(),
-    author=get_authors(),
-    license="New BSD License",
-    author_email = "mike@axiak.net",
-    url = "http://github.com/axiak/pyre2/",
-    ext_modules = [Extension("re2",
-                             ext_files,
-                             language="c++",
-                             include_dirs=[os.path.join(re2_prefix, "include")],
-                             libraries=["re2"],
-                             library_dirs=[os.path.join(re2_prefix, "lib")],
-                             runtime_library_dirs=[os.path.join(re2_prefix, "lib")],
-                             )],
-    cmdclass=cmdclass,
-    classifiers = [
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Cython',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
-    )
+def main():
+    setup(
+        name="re2",
+        version="0.2.20",
+        description="Python wrapper for Google's RE2 using Cython",
+        long_description=get_long_description(),
+        author=get_authors(),
+        license="New BSD License",
+        author_email = "mike@axiak.net",
+        url = "http://github.com/axiak/pyre2/",
+        ext_modules = [Extension("re2",
+                                 ext_files,
+                                 language="c++",
+                                 include_dirs=[os.path.join(re2_prefix, "include")],
+                                 libraries=["re2"],
+                                 library_dirs=[os.path.join(re2_prefix, "lib")],
+                                 runtime_library_dirs=[os.path.join(re2_prefix, "lib")],
+                                 )],
+        cmdclass=cmdclass,
+        classifiers = [
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Cython',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            ],
+        )
+
+if __name__ == '__main__':
+    main()
